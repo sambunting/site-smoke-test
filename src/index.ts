f0020e82eb0697907f12dd8b072df11a3ac61681
Sam Bunting <sambunting2008@live.co.uk>
2022-09-23 22:39:49 +0100

    feat: improved error capture

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ interface IOptions {
 }
 
 const options: IOptions = {
-  sitemapURL: 'https://sam.bunting.dev/sitemap/sitemap-index.xml',
+  sitemapURL: 'https://dazzling-sherbet-63f829.netlify.app/sitemap.xml',
   requestBlacklist: [
     'googletagmanager.com',
     'google-analytics.com'
@@ -16,6 +16,7 @@ const options: IOptions = {
 
 (async () => {
   const urls = (await getURLs(options.sitemapURL))
+  const results = [];
 
   console.log(`Found a total of ${urls.length} urls`)
 
@@ -29,20 +30,45 @@ const options: IOptions = {
     await page.route(`/${url}/g`, route => route.abort());
   }
 
-  page.on('console', msg => {
+  let currentTest: any = { result: 'pass' };
+
+  // Handler for if `console.error` is used
+  await page.on('console', msg => {
     if (msg.type() === 'error')
+      currentTest.result = 'fail';
+
       console.log(`Error text: "${msg.text()}"`);
   });
+
+  // Handler for if there is an uncaught error
+  await page.on('pageerror', msg => {
+    currentTest.result = 'fail'
+
+    console.log('uncaught error', msg.message)
+  })
 
   for (const url of urls) {
     console.log('Testing page', url);
 
+    currentTest.url = url;
+
     await page.goto(url);
     await page.waitForLoadState('networkidle');
+
+    results.push({ ...currentTest });
+    currentTest = Object.assign({ result: 'pass' }, {});
   }
 
-  console.log('Testing complete, beginning teardown...')
+  console.log('Testing complete, beginning teardown...');
 
   await context.close();
   await browser.close();
+
+  const overallPass = results.filter((x) => x.result === 'fail').length > 0 ? false : true;
+
+  if (overallPass) {
+    console.log('All tests completed successfully!')
+  } else {
+    console.log('FAIL');
+  }
 })();
