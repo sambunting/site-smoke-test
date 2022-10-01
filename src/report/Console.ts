9fc8cd777728c983a12dd1c6747a4f938092085f
Sam Bunting <sambunting2008@live.co.uk>
2022-10-01 13:02:23 +0100

    feat: added chalk to console report

--- src/report/Console.ts
+++ src/report/Console.ts
@@ -1,5 +1,6 @@
 import Test from "../Test";
 import BaseReport from "./BaseReport";
+import chalk from "chalk";
 
 class ConsoleReport extends BaseReport {
   constructor(data: Test[]) {
@@ -12,20 +13,24 @@ class ConsoleReport extends BaseReport {
   }
 
   overview = () => {
-    this.counts.pass && console.log(`${this.counts.pass} passes`);
-    this.counts.fail && console.log(`${this.counts.fail} failures`);
+    this.counts.pass && console.log(chalk.green.bold(`${this.counts.pass} passes`));
+    this.counts.fail && console.log(chalk.red.bold(`${this.counts.fail} failures`));
     console.log('');
   }
 
   detail = () => {
     this.data.forEach(({ result, url, duration, errors }) => {
-      console.log(`${result.toUpperCase()} - ${url} ${duration}ms`)
+      const formatResult = result === 'fail' ?
+        chalk.bgRed.white(result.toUpperCase()) :
+        chalk.bgGreen.white(result.toUpperCase())
+
+      console.log(`${formatResult} - ${url} ${chalk.yellow(`${duration}ms`)}`)
 
       if (errors.length !== 0) {
-        console.log(`       Discovered ${errors.length} errors`);
+        console.log(chalk.red(`       Discovered ${errors.length} errors`));
 
         errors.forEach(({ text }, index) => {
-          console.log(`       ${index + 1}. ${text}`);
+          console.log(chalk.red(`       ${index + 1}. ${text}`));
         })
       }
     })
